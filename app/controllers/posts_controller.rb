949da6a0c21f77a099db24ba08b0f2ce263df71e
Naokichi55 <ibosanganba@gmail.com>
2023-09-03 21:15:08 +0900

    編集機能を実装。一覧表示時にログが表示される不具合解消。他機能について追加しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,17 +17,24 @@ class PostsController < ApplicationController
   end
 
   def edit
+
+    @post = Post.find(params[:id])
+
   end
 
   def update
+
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post
   end
 
-  def deestroy
+  def destroy
 
-    post = Post.find(paramas[:id])
+    post = Post.find(params[:id])
     post.destroy!
     redirect_to root_path
-    
+
   end
 
   private
